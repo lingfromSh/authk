39a540db6b2015df1e22aa1998f60d36eaee2051
Shiyun Ling <shiyun.ling@flexiv.com>
2023-09-21 13:39:43 +0800

    chore: init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# AuthK
+
+A full-featured and out-of-box microservice which provides authn and authz.
---
+++ server.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="AuthK",
+    description="A full-featured and out-of-box microservice which provides authn and authz.",
+)
